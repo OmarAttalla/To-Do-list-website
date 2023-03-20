39d0ae8b70aa7eaf2f98ef6da8e2c53c1f785b42
Omar Attalla <omarattalla1@gmail.com>
2023-03-20 22:13:59 +0100

    Make the to-do button functional

---
+++ app.js
@@ -0,0 +1,38 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//EventListeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event){
+    event.preventDefault(); //prevents form from submitting
+    
+    //To-do item div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "Wassup"
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+    //add checkmark button
+    const checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.classList.add("complete-btn");
+    todoDiv.appendChild(checkButton);
+
+    //add trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append todo div to todolist
+    todoList.appendChild(todoDiv);
+
+}
